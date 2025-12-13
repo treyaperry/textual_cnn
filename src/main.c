3374a3ab239829a034ced05310e461993a6060c4
Drew Rife <drew.rife95@gmail.com>
2025-12-13 16:11:12 -0500

    docs: add main function comment

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+///
+/// @brief Main entry point of the program.
+/// @param argc Argument count.
+/// @param argv Argument vector.
+/// @return Exit status code.
+///
 int main(const int argc, const char **argv) {
   printf("Hello, World!\n");
   return 0;
