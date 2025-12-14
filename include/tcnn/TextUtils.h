91333db3537c0c36718087997fa596ba9c5104ec
Drew Rife <drew.rife95@gmail.com>
2025-12-14 15:56:15 -0500

    feat: add cplusplus support for unittesting

--- include/tcnn/TextUtils.h
+++ include/tcnn/TextUtils.h
@@ -5,8 +5,13 @@
 #ifndef TCNN_UTILS_TEXT_H
 #define TCNN_UTILS_TEXT_H
 
+#include <stddef.h>
 #include <stdbool.h>
 #include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
 
 typedef int8_t image_id;
 
@@ -26,7 +31,7 @@ enum TextConstants : image_id {
 ///
 inline bool TextUtils_is_printable_char(const uint8_t CODE) {
   return (CODE >= TEXT_CONSTANTS_FIRST_PRINTABLE_CHAR &&
-          CODE <= TEXT_CONSTANTS_LAST_PRINTABLE_CHAR) != 0;
+          CODE <= TEXT_CONSTANTS_LAST_PRINTABLE_CHAR);
 }
 
 ///
@@ -43,5 +48,18 @@ inline image_id TextUtils_char_to_id(const char CHARACTER) {
   }
   return TEXT_CONSTANTS_PAD_ID; // Padding character ID
 }
+
+///
+/// @brief Converts a string of text into a grid of character IDs.
+/// @param text The input text string.
+/// @param width The width of the grid (number of characters)
+/// @param maxRows The maximum number of rows in the grid.
+///
+/// @todo Implement this function - place holder for now.
+void TextUtils_to_grid_ids(const char* text, size_t width, size_t maxRows);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
 
 #endif // TCNN_UTILS_TEXT_H
