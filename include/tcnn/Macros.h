d4ddac78e150a4efdd59f02a2ce553a2334c8627
Drew Rife <drew.rife95@gmail.com>
2025-12-24 14:57:31 -0500

    feat: create general purpose macros
    header

---
+++ include/tcnn/Macros.h
@@ -0,0 +1,13 @@
+///
+/// @par General-purpose utility macros.
+///
+
+#ifndef TCNN_MACROS_H
+#define TCNN_MACROS_H
+
+///
+/// @brief Get the number of elements in a statically-allocated array.
+///
+#define TCNN_ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
+#endif // TCNN_MACROS_H
