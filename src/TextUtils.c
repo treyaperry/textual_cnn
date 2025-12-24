30154a8f80c6df2c7d3422b92d44418d8ecbbd08
Drew Rife <drew.rife95@gmail.com>
2025-12-24 15:26:43 -0500

    feat: add input validation

--- src/TextUtils.c
+++ src/TextUtils.c
@@ -3,10 +3,43 @@
 
 #include <stddef.h>
 
-size_t TextUtils_to_grid_ids(const TextGridParams *PARAMS, image_id *outIds,
-                             size_t outIdsSize) {
-  TCNN_ASSERT_OR_RETURN_VALUE(PARAMS != NULL, 0, "PARAMS is NULL");
-  TCNN_ASSERT_OR_RETURN_VALUE(PARAMS->text != NULL, 0, "PARAMS->text is NULL");
-  TCNN_ASSERT_OR_RETURN_VALUE(outIds != NULL, 0, "outIds is NULL");
+enum WrittenElements : size_t {
+  ZERO_WRITTEN_ELEMENTS = 0,
+};
+
+///
+/// @brief To grid ids implementation.
+///
+/// This function is called when all input validation checks have passed.
+///
+/// @param PARAMS Parameters for the text grid conversion.
+/// @param outIds Output array to hold the character IDs.
+/// @param outIdsCapacity Capacity of the output array.
+///
+/// @return The number of character IDs written to the output array.
+///
+static size_t TextUtils_to_grid_ids_impl(const TextGridParams *PARAMS,
+                                         image_id *outIds,
+                                         size_t outIdsCapacity) {
   return 0;
+}
+
+size_t TextUtils_to_grid_ids(const TextGridParams *PARAMS, image_id *outIds,
+                             size_t outIdsCapacity) {
+  TCNN_ASSERT_OR_RETURN_VALUE(PARAMS != NULL, ZERO_WRITTEN_ELEMENTS,
+                              "PARAMS is NULL");
+  TCNN_ASSERT_OR_RETURN_VALUE(PARAMS->text != NULL, ZERO_WRITTEN_ELEMENTS,
+                              "PARAMS->text is NULL");
+  TCNN_ASSERT_OR_RETURN_VALUE(outIds != NULL, ZERO_WRITTEN_ELEMENTS,
+                              "outIds is NULL");
+  TCNN_ASSERT_OR_RETURN_VALUE(PARAMS->width > 0, ZERO_WRITTEN_ELEMENTS,
+                              "PARAMS->width is 0");
+  TCNN_ASSERT_OR_RETURN_VALUE(PARAMS->maxRows > 0, ZERO_WRITTEN_ELEMENTS,
+                              "PARAMS->maxRows is 0");
+  const size_t REQUIRED_CAPACITY = PARAMS->width * PARAMS->maxRows;
+  TCNN_ASSERT_OR_RETURN_VALUE(outIdsCapacity >= REQUIRED_CAPACITY,
+                              ZERO_WRITTEN_ELEMENTS,
+                              "outIdsCapacity is too small");
+
+  return TextUtils_to_grid_ids_impl(PARAMS, outIds, outIdsCapacity);
 }
