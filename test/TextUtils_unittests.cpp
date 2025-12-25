5cde64bda43111b37775b3e50879ce9f28316ce7
Drew Rife <drew.rife95@gmail.com>
2025-12-25 07:43:28 -0500

    test: TextUtils_to_grid_ids

--- test/TextUtils_unittests.cpp
+++ test/TextUtils_unittests.cpp
@@ -2,6 +2,7 @@
 /// @par Tests for TextUtils functions.
 ///
 
+#include "tcnn/Macros.h"
 #include "tcnn/TextUtils.h"
 
 #include <cstdint>
@@ -62,15 +63,113 @@ TEST_P(CharToIdTest, CharToId) {
 }
 
 ///
-/// @brief Placeholder test for TextUtils_to_grid_ids function.
-/// @todo Implement tests once the function is implemented.
+/// @brief Tests TextUtils_to_grid_ids with empty string.
 ///
-TEST(TextUtilsToGridIdsTest, PlaceholderTest) {
-  const TextGridParams PARAMS{.text = "sample text", .width = 1, .maxRows = 2};
-  image_id outIds[2];
+TEST(TextUtilsToGridIdsTest, EmptyString) {
+  const TextGridParams PARAMS{.text = "", .width = 5, .maxRows = 2};
+  image_id outIds[10];
 
-  // placeholder
-  EXPECT_EQ(0, ::TextUtils_to_grid_ids(&PARAMS, outIds, 2));
+  // Empty string should produce 1 row of PADs
+  const size_t RESULT =
+      ::TextUtils_to_grid_ids(&PARAMS, outIds, TCNN_ARRAY_LENGTH(outIds));
+  EXPECT_EQ(5, RESULT); // 1 row * 5 width
+
+  // All should be PAD_ID (0)
+  for (size_t i = 0; i < 5; ++i) {
+    EXPECT_EQ(TEXT_CONSTANTS_PAD_ID, outIds[i]);
+  }
+}
+
+///
+/// @brief Tests TextUtils_to_grid_ids with text that fits in one row.
+///
+TEST(TextUtilsToGridIdsTest, SingleRowFitsExactly) {
+  const TextGridParams PARAMS{.text = "Hi", .width = 5, .maxRows = 2};
+  image_id outIds[10];
+
+  const size_t RESULT =
+      ::TextUtils_to_grid_ids(&PARAMS, outIds, TCNN_ARRAY_LENGTH(outIds));
+  EXPECT_EQ(5, RESULT); // 1 row * 5 width
+
+  // 'H' = 40, 'i' = 49, rest should be PAD (0)
+  EXPECT_EQ(::TextUtils_char_to_id('H'), outIds[0]);
+  EXPECT_EQ(::TextUtils_char_to_id('i'), outIds[1]);
+  EXPECT_EQ(TEXT_CONSTANTS_PAD_ID, outIds[2]);
+  EXPECT_EQ(TEXT_CONSTANTS_PAD_ID, outIds[3]);
+  EXPECT_EQ(TEXT_CONSTANTS_PAD_ID, outIds[4]);
+}
+
+///
+/// @brief Tests TextUtils_to_grid_ids with text that spans multiple rows.
+///
+TEST(TextUtilsToGridIdsTest, MultipleRows) {
+  const TextGridParams PARAMS{.text = "Hello World", .width = 5, .maxRows = 3};
+  image_id outIds[15];
+
+  const size_t RESULT =
+      ::TextUtils_to_grid_ids(&PARAMS, outIds, TCNN_ARRAY_LENGTH(outIds));
+  EXPECT_EQ(15, RESULT); // 3 rows * 5 width
+
+  // Row 0: "Hello"
+  EXPECT_EQ(::TextUtils_char_to_id('H'), outIds[0]);
+  EXPECT_EQ(::TextUtils_char_to_id('e'), outIds[1]);
+  EXPECT_EQ(::TextUtils_char_to_id('l'), outIds[2]);
+  EXPECT_EQ(::TextUtils_char_to_id('l'), outIds[3]);
+  EXPECT_EQ(::TextUtils_char_to_id('o'), outIds[4]);
+  // Row 1: " Worl"
+  EXPECT_EQ(::TextUtils_char_to_id(' '), outIds[5]);
+  EXPECT_EQ(::TextUtils_char_to_id('W'), outIds[6]);
+  EXPECT_EQ(::TextUtils_char_to_id('o'), outIds[7]);
+  EXPECT_EQ(::TextUtils_char_to_id('r'), outIds[8]);
+  EXPECT_EQ(::TextUtils_char_to_id('l'), outIds[9]);
+  // Row 2: "d" + PADs
+  EXPECT_EQ(::TextUtils_char_to_id('d'), outIds[10]);
+  EXPECT_EQ(TEXT_CONSTANTS_PAD_ID, outIds[11]);
+  EXPECT_EQ(TEXT_CONSTANTS_PAD_ID, outIds[12]);
+  EXPECT_EQ(TEXT_CONSTANTS_PAD_ID, outIds[13]);
+  EXPECT_EQ(TEXT_CONSTANTS_PAD_ID, outIds[14]);
+}
+
+///
+/// @brief Tests TextUtils_to_grid_ids with text exceeding maxRows.
+///
+TEST(TextUtilsToGridIdsTest, TruncateAtMaxRows) {
+  const TextGridParams PARAMS{.text = "ABCDEFGHIJ", .width = 3, .maxRows = 2};
+  image_id outIds[10];
+
+  const size_t RESULT =
+      ::TextUtils_to_grid_ids(&PARAMS, outIds, TCNN_ARRAY_LENGTH(outIds));
+  EXPECT_EQ(6, RESULT); // 2 rows * 3 width (truncates after 6 chars)
+
+  // Row 0: "ABC"
+  EXPECT_EQ(::TextUtils_char_to_id('A'), outIds[0]);
+  EXPECT_EQ(::TextUtils_char_to_id('B'), outIds[1]);
+  EXPECT_EQ(::TextUtils_char_to_id('C'), outIds[2]);
+
+  // Row 1: "DEF"
+  EXPECT_EQ(::TextUtils_char_to_id('D'), outIds[3]);
+  EXPECT_EQ(::TextUtils_char_to_id('E'), outIds[4]);
+  EXPECT_EQ(::TextUtils_char_to_id('F'), outIds[5]);
+  // Characters 'G', 'H', 'I', 'J' are truncated
+}
+
+///
+/// @brief Tests TextUtils_to_grid_ids with non-printable characters.
+///
+TEST(TextUtilsToGridIdsTest, NonPrintableChars) {
+  const TextGridParams PARAMS{.text = "A\nB\tC", .width = 5, .maxRows = 2};
+  image_id outIds[10];
+
+  const size_t RESULT =
+      ::TextUtils_to_grid_ids(&PARAMS, outIds, TCNN_ARRAY_LENGTH(outIds));
+  EXPECT_EQ(5, RESULT); // 1 row * 5 width
+
+  // 'A', PAD(\n), 'B', PAD(\t), 'C'
+  EXPECT_EQ(::TextUtils_char_to_id('A'), outIds[0]);
+  EXPECT_EQ(TEXT_CONSTANTS_PAD_ID, outIds[1]); // '\n' -> PAD
+  EXPECT_EQ(::TextUtils_char_to_id('B'), outIds[2]);
+  EXPECT_EQ(TEXT_CONSTANTS_PAD_ID, outIds[3]); // '\t' -> PAD
+  EXPECT_EQ(::TextUtils_char_to_id('C'), outIds[4]);
 }
 
 ///
