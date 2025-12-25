3d1a8dfa03190823a1465316674348d4f506f27b
Drew Rife <drew.rife95@gmail.com>
2025-12-25 13:54:10 -0500

    refactor: convert to C++

---
+++ src/TextUtils.cpp
@@ -0,0 +1,7 @@
+#include "tcnn/TextUtils.hpp"
+
+auto TextUtils_to_grid_ids(const TextGridParams &PARAMS) noexcept -> void {
+  // Placeholder implementation
+  // Actual implementation to convert text to grid of character IDs goes here
+  static_cast<void>(PARAMS);
+}
