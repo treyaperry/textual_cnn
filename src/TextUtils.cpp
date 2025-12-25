5b21a6da6ce29289958d37cb1556a9187d2d5ffb
Drew Rife <drew.rife95@gmail.com>
2025-12-25 14:17:24 -0500

    refactor: initial commit to c++

--- src/TextUtils.cpp
+++ src/TextUtils.cpp
@@ -1,7 +1,11 @@
 #include "tcnn/TextUtils.hpp"
 
+namespace tcnn {
+
 auto TextUtils_to_grid_ids(const TextGridParams &PARAMS) noexcept -> void {
   // Placeholder implementation
   // Actual implementation to convert text to grid of character IDs goes here
   static_cast<void>(PARAMS);
 }
+
+} // namespace tcnn
