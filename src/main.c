e0f65d059825a3ac494661c961c7647343a87038
Drew Rife <drew.rife95@gmail.com>
2025-12-25 08:25:24 -0500

    feat: parse command line arguments
    to open csv file

--- src/main.c
+++ src/main.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "tcnn/ErrorHandling.h"
+
+static void print_usage() {
+  // NOLINTBEGIN(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling)
+  fprintf(stderr, "Usage: tcnn <path_to_csv_file>\n");
+  fprintf(stderr, "Example: tcnn data/textual_cnn/training/AI_Human.csv\n");
+  // NOLINTEND(clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling)
+}
 
 ///
 /// @brief Main entry point of the program.
@@ -7,6 +17,16 @@
 /// @return Exit status code.
 ///
 int main(const int argc, const char **argv) {
-  printf("Hello, World!\n");
-  return 0;
+  if (argc <= 1) {
+    print_usage();
+    return EXIT_FAILURE;
+  }
+
+  const char *csvPath = argv[1];
+  FILE *csvFile = fopen(csvPath, "r");
+  TCNN_ASSERT_OR_RETURN_VALUE(csvFile != NULL, EXIT_FAILURE,
+                              "Failed to open CSV file");
+  fclose(csvFile);
+
+  return EXIT_SUCCESS;
 }
