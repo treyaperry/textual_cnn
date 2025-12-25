38743dea0fa52b4789e0bf14cddec6089594117b
Drew Rife <drew.rife95@gmail.com>
2025-12-25 14:40:35 -0500

    feat: create error hadnling and macro header

---
+++ include/tcnn/Macros.h
@@ -0,0 +1,14 @@
+///
+/// @par General-purpose utility macros.
+///
+
+#ifndef TCNN_MACROS_H
+#define TCNN_MACROS_H
+
+///
+/// @brief Stringify a macro argument.
+///
+#define TCNN_INTERNAL_STRINGIFY(x) #x
+#define TCNN_STRINGIFY(x) TCNN_INTERNAL_STRINGIFY(x)
+
+#endif // TCNN_MACROS_H
