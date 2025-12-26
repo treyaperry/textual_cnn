e21419582b47fa69d0dabe6c838d2735b962e83a
Drew Rife <drew.rife95@gmail.com>
2025-12-26 14:12:38 -0500

    feat: update with nodiscard

--- src/TextUtils.cpp
+++ src/TextUtils.cpp
@@ -8,11 +8,11 @@ auto to_grid_ids(const TextGridParams &params) noexcept
     -> std::vector<ImageId> {
   TCNN_ASSERT_OR_RETURN_VALUE(params.width > 0, std::vector<ImageId>{},
                               "Width must be greater than zero.");
-  TCNN_ASSERT_OR_RETURN_VALUE(params.maxRows > 0, std::vector<ImageId>{},
+  TCNN_ASSERT_OR_RETURN_VALUE(params.max_rows > 0, std::vector<ImageId>{},
                               "Max rows must be greater than zero.");
 
   // Preallocate the output vector with padding IDs.
-  std::vector<ImageId> ids(params.width * params.maxRows, text_pad_id);
+  std::vector<ImageId> ids(params.width * params.max_rows, text_pad_id);
   return ids;
 }
 
